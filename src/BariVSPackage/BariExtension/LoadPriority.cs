ae07512d096af315849d5c3f68d802a3b5f613d9
Zoltan Tibor <Zoltan.Tibor@unknown.local>
2015-08-14 13:15:39 +0200

    LoadPriority added
    (Converted from Mercurial changeset 8c98bb3f3694, rev 17)

---
+++ src/BariVSPackage/BariExtension/LoadPriority.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOTEM.BariVSPackage.BariExtension
+{
+    public enum LoadPriority
+    {
+        DemandLoad,
+        BackgroundLoad,
+        LoadIfNeeded,
+        ExplicitLoadOnly,
+    }
+}
