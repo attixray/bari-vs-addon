2f5236abf86841026375104d73327c20fd858d60
Zoltan Tibor <Zoltan.Tibor@unknown.local>
2015-09-13 20:33:42 +0200

    SolutionWatcher improved because of VS
    (Converted from Mercurial changeset 2582a479862b, rev 23)

--- src/BariVSPackage/BariExtension/SolutionWatcher.cs
+++ src/BariVSPackage/BariExtension/SolutionWatcher.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Timers;
 
 namespace KOTEM.BariVSPackage.BariExtension
 {
@@ -20,6 +21,11 @@ namespace KOTEM.BariVSPackage.BariExtension
         private FileSystemWatcher watcher;
         private HashSet<string> extensions = new HashSet<string>(new [] { ".cs", ".fs", ".xaml", ".cpp", ".xml", ".h", ".c" });
         private HashSet<string> projExtensions = new HashSet<string>(new[] {".yaml", ".csproj", ".vcxproj", ".fsproj", ".vcproj" });
+        private Timer deleteTimer;
+        private readonly IList<string> deletedFiles = new List<string>(); 
+
+        public event EventHandler Changed;
+        public event EventHandler<ReloadEventArgs> ReloadNeeded;
 
         public SolutionWatcher(string srcDir)
         {
@@ -33,11 +39,14 @@ namespace KOTEM.BariVSPackage.BariExtension
                                              | NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.Size
                           };
             watcher.Changed += FileSystemChanged;
-            watcher.Deleted += FileSystemChanged;
+            watcher.Deleted += FileSystemChangedDelRename;
             watcher.Created += FileSystemChanged;
-            watcher.Renamed += FileSystemChanged;
-        }
+            watcher.Renamed += FileSystemChangedDelRename;
 
+            deleteTimer = new Timer(50);
+            deleteTimer.Elapsed += deleteTimerOnElapsed;
+        }
+        
         private void FileSystemChanged(object sender, FileSystemEventArgs e)
         {
             if (Changed != null)
@@ -47,12 +56,10 @@ namespace KOTEM.BariVSPackage.BariExtension
                 if (extensions.Contains(ext))
                     Changed(this, EventArgs.Empty);
 
-                if ((e.ChangeType == WatcherChangeTypes.Created ||
-                    e.ChangeType == WatcherChangeTypes.Deleted ||
-                    e.ChangeType == WatcherChangeTypes.Renamed) && extensions.Contains(ext))
+                if (e.ChangeType == WatcherChangeTypes.Created && extensions.Contains(ext))
                 {
                     if (ReloadNeeded != null)
-                        ReloadNeeded(this, new ReloadEventArgs(e.FullPath){ReBuildNeeded = true});
+                        ReloadNeeded(this, new ReloadEventArgs(e.FullPath) {ReBuildNeeded = true});
                 }
 
                 if (e.ChangeType == WatcherChangeTypes.Changed && projExtensions.Contains(ext))
@@ -63,13 +70,54 @@ namespace KOTEM.BariVSPackage.BariExtension
             }
         }
 
-        public event EventHandler Changed;
-        public event EventHandler<ReloadEventArgs> ReloadNeeded;
+        private void FileSystemChangedDelRename(object sender, FileSystemEventArgs e)
+        {
+            var ext = (Path.GetExtension(e.FullPath) ?? string.Empty).ToLower();
+
+            if (e.ChangeType == WatcherChangeTypes.Deleted)
+            {
+                deletedFiles.Add(e.FullPath);
+                deleteTimer.Stop();
+                deleteTimer.Start();
+            }
+
+            if (e.ChangeType == WatcherChangeTypes.Renamed && extensions.Contains(ext))
+            {
+                var fakeDeletes = deletedFiles.Where(d => Path.GetFileName(d).StartsWith(Path.GetFileName(e.FullPath)));
+                foreach (var fakeDelete in fakeDeletes)
+                {
+                    deletedFiles.Remove(fakeDelete);
+                }
+            }
+        }
+        private void deleteTimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            deleteTimer.Stop();
+
+            foreach (var deletedFile in deletedFiles.Where(f => extensions.Contains((Path.GetExtension(f) ?? string.Empty).ToLower())))
+            {
+                if (ReloadNeeded != null)
+                    ReloadNeeded(this, new ReloadEventArgs(deletedFile) { ReBuildNeeded = true });    
+            }
+
+            deletedFiles.Clear();
+        }
 
         public void Dispose()
         {
-            watcher.Dispose();
-            watcher = null;
+            if (watcher != null)
+            {
+                watcher.Dispose();
+                watcher = null;
+            }
+
+            if (deleteTimer != null)
+            {
+                deleteTimer.Stop();
+                deleteTimer.Elapsed -= deleteTimerOnElapsed;
+                deleteTimer.Dispose();
+                deleteTimer = null;
+            }
         }
     }
 }
